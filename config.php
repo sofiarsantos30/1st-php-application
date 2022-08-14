c6c3806bf1949c5cdf7a8f88f6d676247fd50c5b
Sofia Santos <sofiarsantos22@hotmail.com>
2022-08-14 22:49:00 +0100

    backend index

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+define('USER_LOGIN', 'admin');
+define('USER_PASSWORD', '123456');
+define('PAGE_TITLE', 'Exercício PHP N.03 - Login e Logout');
---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+require_once 'config.php';
+require_once 'functions/url.php';
+require_once 'templates/head.php';
+
+if (empty($_GET['route'])) {
+    $page = 'login';
+} else {
+    $page= $_GET['route'];
+}
+
+switch($page) {
+    case 'dashboard':
+        require_once 'controllers/dashboard.php';
+        break;
+    case 'authenticate':
+        require_once 'controllers/authenticate.php';
+        break;
+    case 'logout':
+        require_once 'controllers/logout.php';
+        break;
+    
+    default:
+        break;
+}
+
+$page_template = 'templates/page_' . $page . '.php';
+
+require_once 'templates/head.php';
+
+if(file_exists($page_template)) {
+    require_once $page_template;
+} else {
+    require_once 'templates/page_not_found.php';
+}
+
+require_once 'templates/foot.php';
